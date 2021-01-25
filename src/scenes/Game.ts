88e401249c04944aa7ac64468cdab87302b591b2
Kevin Zieber <zieber.kevinr@gmail.com>
2021-01-25 08:30:24 -0800

    first scene tiles done

---
+++ src/scenes/Game.ts
@@ -0,0 +1,18 @@
+import Phaser from 'phaser'
+
+export default class Game extends Phaser.Scene {
+	constructor()
+	{
+		super('game')
+	}
+
+	preload()
+    {
+
+    }
+
+    create()
+    {
+
+    }
+}
